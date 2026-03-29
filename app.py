d1dd71a2e76c5a42d83c385e579cd257c6d6214c
Sebi Radut <sebi.radut@icloud.com>
2026-03-29 18:46:59 +0300

    serve frontend from flask

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 from scanner import scan
+import os
 
 app = Flask(__name__)
 CORS(app)
+
+@app.route("/")
+def index():
+    return send_from_directory('.', 'index.html')
 
 @app.route("/scan", methods=["GET"])
 def scan_url():
